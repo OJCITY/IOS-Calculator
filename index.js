c4672b0355fcfad71d4e0a263cfa341318bc2f7e
SEGUN OJO <ojcity_22@yahoo.com>
2022-12-17 11:53:41 +0100

    IOS calculator deployment

---
+++ index.js
@@ -0,0 +1,69 @@
+// This code helps to prevent the page from reloading anytime it is clicked
+const form = document.getElementById("calc_form");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+});
+
+//Lets start adding functionality to the calculator
+const output = document.getElementById("output");
+const operand_btns = document.querySelectorAll("button[data-type=operand]");
+
+let is_operator = false;
+
+const remove_active = () => {
+  operator_btns.forEach((btn) => {
+    btn.classList.remove("active");
+  });
+};
+
+operand_btns.forEach((btn) => {
+  btn.addEventListener("click", (e) => {
+    remove_active();
+    if (output.value == "0") {
+      output.value = e.target.value;
+    } else if (output.value.includes(".")) {
+      output.value = output.value + "" + e.target.value.replace(".", "");
+    } else if (is_operator) {
+      is_operator = false;
+      output.value = e.target.value;
+    } else {
+      output.value = output.value + "" + e.target.value;
+    }
+  });
+});
+
+//Next
+const operator_btns = document.querySelectorAll("button[data-type=operator]");
+
+let equation = [];
+operator_btns.forEach((btn) => {
+  btn.addEventListener("click", (e) => {
+    e.currentTarget.classList.add("active");
+
+    switch (e.target.value) {
+      case "%":
+        output.value = parseFloat(output.value) / 100;
+        break;
+      case "invert":
+        output.value = parseFloat(output.value) * -1;
+        break;
+      case "=":
+        equation.push(output.value);
+        output.value = eval(equation.join(""));
+        equation = [];
+        break;
+      default:
+        let last_item = equation[equation.length - 1];
+        if (["/", "*", "+", "-"].includes(last_item) && is_operator) {
+          equation.pop();
+          equation.push(e.target.value);
+        } else {
+          equation.push(output.value);
+          equation.push(e.target.value);
+        }
+        is_operator = true;
+        break;
+    }
+  });
+});
